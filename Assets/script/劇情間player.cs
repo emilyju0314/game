929075fcaed5cdc8d2dc4d2d13d54f7c4c349bc6
emilyju0314 <103551088+emilyju0314@users.noreply.github.com>
2023-01-28 16:59:52 +0800

    劇情間 限制玩家走動
    走到盡頭時無法自由走動

---
+++ Assets/script/劇情間player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 劇情間player : MonoBehaviour
+{
+    private float HorizontInput;
+    private float VerticalInput;
+    private int run;
+    private Vector3 startPosition = new Vector3(0, -4, 0);
+    private bool ani = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = 12;
+        }
+        else
+        {
+            run = 10;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!ani)
+            transform.position += new Vector3(HorizontInput * run * Time.deltaTime, VerticalInput * run * Time.deltaTime);
+        else if(transform.position.y<11)
+            transform.position += new Vector3(0, Time.deltaTime);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ani = true;
+    }
+
+}
