a2697dc708ccee6805765bf3954513506a031074
emilyju0314 <103551088+emilyju0314@users.noreply.github.com>
2023-02-05 15:55:02 +0800

    起始頁面 開始時增加淡入

--- Assets/script/start.cs
+++ Assets/script/start.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class start : MonoBehaviour
 {
     GameObject background;
+    bool start_ = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +15,23 @@ public class start : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (start_ == true)
+        {
+            background.GetComponent<SpriteRenderer>().color = Color.Lerp(background.GetComponent<SpriteRenderer>().color, Color.black, Time.deltaTime * 0.5f);
+        }
     }
 
     public void startClick()
     {
-        background.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+        start_ = true;
         GameObject.Find("起始文字").GetComponent<Text>().enabled = true;
         Debug.Log("可以");
-        GameObject.Find("exit").GetComponent<Button>().enabled = false;
-        GameObject.Find("start").GetComponent<Button>().enabled = false;
-        Invoke("changeScene", 2);
-       // changeScene();
+        GameObject.Find("exit").GetComponentInChildren<Text>().enabled = false;
+        GameObject.Find("start").GetComponentInChildren<Text>().enabled = false;
+        Invoke("changeScene", 6);
     }
     public void exitClick()
     {
-        Debug.Log("肯定");
         Application.Quit();
     }
     void changeScene()
