df6642c723aa8f156837ebf01d18eaf5feaba384
emilyju0314 <103551088+emilyju0314@users.noreply.github.com>
2023-02-07 06:44:01 +0800

    增加左右跑步動畫+禁止旋轉

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -8,12 +8,13 @@ public class Player : MonoBehaviour
     private float VerticalInput;
     private int run;
     private Vector3 startPosition = new Vector3(-15, 0, 0);
-
+    Animator anime;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = startPosition;
+        anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,14 @@ public class Player : MonoBehaviour
     {
         HorizontInput = Input.GetAxis("Horizontal");
         VerticalInput = Input.GetAxis("Vertical");
+
+        anime.SetFloat("x", HorizontInput);//判定player奔跑方向
+
+        if (HorizontInput == 0)
+            anime.SetBool("x0", true);//判定player朝向
+        else
+            anime.SetBool("x0", false);
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             run = 12;
--- Assets/script/劇情間player.cs
+++ Assets/script/劇情間player.cs
@@ -7,13 +7,14 @@ public class 劇情間player : MonoBehaviour
     private float HorizontInput;
     private float VerticalInput;
     private int run;
-    private Vector3 startPosition = new Vector3(0, -4, 0);
+    private Vector3 startPosition = new Vector3(0, -3, 0);
     private bool ani = false;
-
+    Animator anime;
     // Start is called before the first frame update
     void Start()
     {
         transform.position = startPosition;
+        anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +22,18 @@ public class 劇情間player : MonoBehaviour
     {
         HorizontInput = Input.GetAxis("Horizontal");
         VerticalInput = Input.GetAxis("Vertical");
+
+        anime.SetFloat("x", HorizontInput);//判定player奔跑方向
+
+        if (HorizontInput == 0)
+            anime.SetBool("x0", true);//判定player朝向
+        else
+            anime.SetBool("x0", false);
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             run = 12;
+
         }
         else
         {
